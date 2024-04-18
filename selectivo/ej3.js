978511a0b35bfcefe941e1b815a22565edb3c60f
fabyup <afabyblasco@gmail.com>
2024-04-17 23:22:22 -0300

    este es el ejercicio 3 selectivo

---
+++ selectivo/ej3.js
@@ -0,0 +1,19 @@
+const leer = require("prompt-sync")();
+
+console.log("ingresa dos numeros y los vamos a sumar +");
+let numeroIngresado1=Number (leer());
+let numeroIngresado2= Number (leer());
+let sumaDeNumerosIngresados= numeroIngresado1 +numeroIngresado2;
+console.log("la sumas es", sumaDeNumerosIngresados);
+
+
+
+
+
+
+
+
+
+
+
+
