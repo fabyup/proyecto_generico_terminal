2854bffc22e7b6164a0384cd0277adc3b8c73402
fabyup <afabyblasco@gmail.com>
2024-04-20 20:37:47 -0300

    Este es el ejercicio 4 iterativo

--- app.js
+++ app.js
@@ -1,2 +1,1 @@
 const leer = require("prompt-sync")();
-
---
+++ iterativo/ej4.js
@@ -0,0 +1,9 @@
+
+const leer = require("prompt-sync")();
+
+console.log("ingresar mensaje");
+let mensajeIngresado = "hola mundo";
+
+for(mensajeIngresado=0 ; mensajeIngresado<5;mensajeIngresado ++){
+    console.log("hola mundo", mensajeIngresado +1);
+}
