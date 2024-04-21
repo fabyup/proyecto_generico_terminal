5736b28f9e3d1d70e3beb3ea5d91e55ecd20bfca
fabyup <afabyblasco@gmail.com>
2024-04-20 22:43:26 -0300

    este es el ejercicio 4-1 iterativo

---
+++ iterativo/ej4-1.js
@@ -0,0 +1,21 @@
+
+const leer = require("prompt-sync")();
+
+
+console.log("ingresa un numero");
+let num1= Number( leer());
+
+console.log("la sumatoria es...", + num1 );
+let num2= Number(leer());
+console.log("la sumatoria es..",+ num1 + num2);
+let num3=Number( leer());
+console.log("la sumatoria es..",+ num1 + num2 +num3);
+let num4=Number( leer());
+console.log("la sumatoria es..",+ num1 + num2+num3+num4);
+
+let num5=Number( leer());
+console.log("la sumatoria es..",+ num1 + num2+num3+num4+num5);
+
+
+
+
