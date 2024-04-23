36645e8433a0278977c0517067ad5d35f496583e
fabyup <afabyblasco@gmail.com>
2024-04-23 20:58:57 -0300

    este es el ejercico 6 modificado

--- iterativo/ej6.js
+++ iterativo/ej6.js
@@ -4,8 +4,10 @@ const leer = require("prompt-sync")();
 
 const NUMERO_SECRETO = 7
 
-for(let numerosIngresados= 0 ; numerosIngresados<10 ; numerosIngresados++){
+for(let contador= 0 ; contador<10 ; contador++){
+    let numerosIngresados = Number (leer())
      if(numerosIngresados==NUMERO_SECRETO) {
+        contador=10;
         console.log("ganaste");
      }else{
         console.log("perdiste");
